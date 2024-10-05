7a57646ed626039c72f1524150097345a18624a4
duckbei <didicomputing@gmail.com>
2024-10-05 18:00:05 +0200

    Added version print for debugging purposes

--- src/main/java/me/didi/PWMBackend/PwmBackendApplication.java
+++ src/main/java/me/didi/PWMBackend/PwmBackendApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import lombok.extern.slf4j.Slf4j;
+
 @EnableScheduling
+@Slf4j
 @SpringBootApplication
 public class PwmBackendApplication {
 	public static void main(String[] args) {
+		log.info("Running version with email provider");
 		SpringApplication.run(PwmBackendApplication.class, args);
 	}
 }
